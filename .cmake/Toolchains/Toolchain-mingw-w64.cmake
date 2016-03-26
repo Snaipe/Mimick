5e3aca9ba3be44f4c680ed66620dc7c62d274c76
Snaipe <franklinmathieu@gmail.com>
2016-03-26 09:00:07 -0700

    mingw: Fixed the toolchain file when cross compiling on windows

--- .cmake/Toolchains/Toolchain-mingw-w64.cmake
+++ .cmake/Toolchains/Toolchain-mingw-w64.cmake
@@ -1,5 +1,5 @@
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME Windows)
 
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "^x86")
     set (CMAKE_SYSTEM_PROCESSOR i686)
@@ -10,23 +10,22 @@ elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^x64")
 endif ()
 
 if (WIN32)
-    SET(CMAKE_C_COMPILER "C:/msys64/mingw${BITS}/bin/gcc.exe")
-    SET(CMAKE_CXX_COMPILER "C:/msys64/mingw${BITS}/bin/g++.exe")
-    SET(CMAKE_RC_COMPILER "C:/msys64/mingw${BITS}/bin/windres.exe")
-    SET(CMAKE_FIND_ROOT_PATH "C:/msys64/mingw${BITS}" "C:/msys64/mingw${BITS}/${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32")
+    set(CMAKE_FIND_ROOT_PATH "C:/msys64/mingw${BITS}" "C:/msys64/mingw${BITS}/${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32")
+    set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+    set(ENV{PATH} "C:\\msys64\\mingw${BITS}\\bin;C:\\msys64\\mingw${BITS}\\${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32\\bin;$ENV{PATH}")
 else ()
-    # which compilers to use for C and C++
-    SET(CMAKE_C_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-gcc)
-    SET(CMAKE_CXX_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-g++)
-    SET(CMAKE_RC_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-windres)
-
-    # here is the target environment located
-    SET(CMAKE_FIND_ROOT_PATH /usr/${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32)
+    set(CMAKE_FIND_ROOT_PATH /usr/${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32)
+    set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 endif ()
+
+# which compilers to use for C and C++
+set(CMAKE_AR ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-gcc-ar CACHE FILEPATH "Archiver")
+set(CMAKE_C_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-windres)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
