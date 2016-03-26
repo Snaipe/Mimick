d96256ff32fe15f58d070acc4ab84582a3e9b0ae
Snaipe <franklinmathieu@gmail.com>
2016-03-26 01:56:51 +0100

    appveyor: Fixed builds not using the correct architecture

--- .cmake/Toolchains/Toolchain-mingw-w64.cmake
+++ .cmake/Toolchains/Toolchain-mingw-w64.cmake
@@ -1,14 +1,19 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
-SET(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "^x86")
+    set (CMAKE_SYSTEM_PROCESSOR i686)
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^x64")
+    set (CMAKE_SYSTEM_PROCESSOR x86_64)
+endif ()
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+SET(CMAKE_C_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+SET(CMAKE_FIND_ROOT_PATH /usr/${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
